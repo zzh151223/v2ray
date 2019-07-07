596b395cb767f378d87920cc9bf940fe6a4f388c
zzh151223 <47099284+zzh151223@users.noreply.github.com>
2019-07-07 20:32:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -754,9 +754,9 @@ install_v2ray() {
 	else
 		pushd /tmp
 		if [[ $_test ]]; then
-			git clone https://github.com/233boy/v2ray -b test /etc/v2ray/233boy/v2ray
+			git clone https://github.com/zzh151223/v2ray -b test /etc/v2ray/233boy/v2ray
 		else
-			git clone https://github.com/233boy/v2ray /etc/v2ray/233boy/v2ray
+			git clone https://github.com/zzh151223/v2ray /etc/v2ray/233boy/v2ray
 		fi
 		popd
 
